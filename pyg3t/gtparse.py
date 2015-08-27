1c806fbc650260a46ef643155cebdfc57ffb1ba1
Kenneth Nielsen <k.nielsen81@gmail.com>
2015-08-27 15:51:06 +0200

    Updated comments in gtparse for sphinx/napoleon (initial commit)

--- pyg3t/gtparse.py
+++ pyg3t/gtparse.py
@@ -20,18 +20,64 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import unicode_literals
 import re
 
-#from textwrap import TextWrapper
-#wrapper = TextWrapper(width=77,
-#                      replace_whitespace=False,
-#                      expand_tabs=False,
-#                      drop_whitespace=False)
+# It is recommended that the license should be the first comment in each source
+# code file, but it doesn't make a good module level doc string, so supply one
+# manually
 
-# Built-in textwrapper doesn't support the drop_whitespace=False
-# option before 2.6, and it's sort of nice to support 2.5 still.
-# So this is sort of equivalent to the TextWrapper
+__doc__ = """
+The gtparse module contains the basic functionality to parse
+:term:`gettext catalog` s (.po files). The most important items are:
+
+ * The :py:func:`.parse` function, which parses an entire gettext catalog from
+   a file like object. **This function is the main entry point for the module**.
+ * The basic types for a single :py:class:`.Message` and for a single
+   :py:class:`.ObsoleteMessage`
+ * The basic type for a :py:class:`.Catalog`, that represents an entire gettext
+   catalogs worth of messages
+
+.. data:: linepatternstrings
+
+    (dict) The regular expression strings used to parse the .po files
+
+.. data:: linepatterns
+
+    (dict) The compiled regular expression objects, corresponding to
+    :py:data:`.linepatternstrings`, used to parse active messages in the .po
+    files
+
+.. data:: obsolete_linepatterns
+
+    (dict)  The compiled regular expression objects, corresponding to
+    :py:data:`.linepatternstrings` with ``'#~( ?)'`` prefixed, used to parse
+    obsolete messages in the .po files
+
+.. data:: wordseparator
+
+    (dict) Compiled regular expression object used as the delimiter for
+    splitting text up into words
+""".lstrip()
 
 
 def chunkwrap(chunks):
+    """Returns a generator of lines, from the content in :term:`chunk` s,
+    wrapped to a max length of 77 characters.
+
+    Similar in functionality to:
+
+    .. code-block:: python
+
+       textwrapper.TextWrapper(width=77,
+                               replace_whitespace=False,
+                               expand_tabs=False,
+                               drop_whitespace=False)
+
+    but since TextWrapper in Python 2.5 does not support the
+    ``drop_whitespace=False`` option, this implementation is used until
+    support for Python 2.5 is dropped.
+
+    Args:
+        chunks (iterable): :term:`Chunk` s of text to wrap
+    """
     tokens = []
     chars = 0
     for chunk in chunks:
@@ -51,11 +97,42 @@ wordseparator = re.compile(r'(\s+)')
 
 
 def wrap(text):
+    """Returns a list of lines, wrapped to a max width of 77 characters,
+    generated from text.
+    """
     chunks = iter(wordseparator.split(text))
     return list(chunkwrap(chunks))
 
 
 def parse_header_data(msg):
+    """Parse the data in the .po file header
+
+    Header data looks like this:
+
+    .. code-block:: python
+
+       Project-Id-Version: nautilus
+       Report-Msgid-Bugs-To: http://bugzilla.gnome.org/enter_bug.cgi?
+       product=nautilus&keywords=I18N+L10N&component=Internationalization (i18n)
+       POT-Creation-Date: 2015-08-26 23:10+0000
+       PO-Revision-Date: 2015-03-14 03:30+0100
+       Last-Translator: Ask Hjorth Larsen <asklarsen@gmail.com>
+       Language-Team: Danish <dansk@dansk-gruppen.dk>
+       Language: da
+       MIME-Version: 1.0
+       Content-Type: text/plain; charset=UTF-8
+       Content-Transfer-Encoding: 8bit
+       Plural-Forms: nplurals=2; plural=(n != 1);
+
+    where each line is a key, value pair of header data separated by the
+    first kolon.
+
+    Args:
+        msg (:py:class:`.Message`): The message that contains the header
+
+    Returns:
+        dict: Key, value pairs of header info. All keys and values are strs.
+    """
     headers = {}
     for line in msg.msgstr.split(r'\n'):
         if not line or line.isspace():
@@ -83,7 +160,24 @@ def _get_header(msgs):
 
 
 class Catalog(object):
-    """A Catalog represents one gettext catalog, or po-file."""
+    """A Catalog represents one gettext catalog, or po-file
+
+    Args:
+        fname (str): Filename of the source
+        encoding (str): The encoding of the source
+        msgs (iterable): Iterable of :py:class:`.Message` or
+            :py:class:`.ObsoleteMessage`
+
+    Attributes:
+        fname (str): The filename of the source
+        encoding (str): The encoding of the source
+        msgs (list): The list of messages (:py:class:`.Message`) in the
+            catalog
+        obsoletes (list): The list of obsolete messages
+            (:py:class:`.ObsoleteMessage`) in the catalog
+        header (:py:class:`.Message`): The message corresponding to the header
+            of the .po file
+    """
     def __init__(self, fname, encoding, msgs):
         self.fname = fname
         self.encoding = encoding
@@ -103,38 +197,59 @@ class Catalog(object):
     def dict(self):
         """Return a dict with the contents of this catalog.
 
-        Values are Messages and keys are tuples of (msgid, msgctxt)."""
+        Values are Messages and keys are tuples of (msgid, msgctxt).
+        """
         d = {}
         for msg in self.msgs:
             d[msg.key] = msg
         return d
     
     def obsoletes(self):
+        """Return an iterator over the obsolete messages"""
         return iter(self.obsoletes)
 
     def __iter__(self):
+        """Return an iterator over the active message"""
         return iter(self.msgs)
 
     def __len__(self):
+        """Return the number of active messages"""
         return len(self.msgs)
 
     def __getitem__(self, index):
+        """Return an item by index from the active messages"""
         return self.msgs[index]
     
     def _catalog(self, msgs, obsoletes=None):
+        """Return a new catalog with same filename and encoding
+
+        Args:
+            msgs (iterable): Messages to include
+            obsoletes (iterable): (Optional) obsoletes to include
+        """
         cat = Catalog(self.fname, self.encoding, msgs, obsoletes)
         return cat
 
     def filter(self, choicefunction):
+        """Return a filtered copy
+
+        Will only contain the messages for which choicefunction return True
+
+        Args:
+            choicefunction (func): A filter function
+        """
         return self._catalog(msg for msg in self if choicefunction(msg))
 
     def get_translated(self):
+        """Return a copy containing only the translated messages"""
         return self.filter(Message.istranslated)
     
     def get_untranslated(self):
+        """Return a copy containing only the untranslated messages"""
         return self.filter(Message.untranslated)
     
     def get_fuzzy(self):
+        """Return a copy containing only the fuzzy messages"""
         return self.filter(Message.isfuzzy)
 
 
@@ -446,6 +561,24 @@ class UnimplementedPoSyntaxError(NotImplementedError):
 
 
 def consume_lines(nextline, input, startpattern, continuepattern):
+    """Consume lines from an input file like object
+
+    Will consume lines from input until the lines no longer matches
+    continuepattern, corresponding to one .po-file chunk.
+
+    Args:
+        nextline (bytes): The pre-read first line of the chunk
+        input (file): Input file like object
+        startpattern (str): Start pattern regular expression
+        continuepattern (str): Continue pattern regular expression
+
+    Returns:
+        (bytes, list): The pre-read next line and a list of lines for the chunk
+
+    Raises:
+        ValueError: If nextline does not match startpattern
+
+    """
     if startpattern.match(nextline) is None:
         raise ValueError('grrr')#BadSyntaxError
     lines = [nextline]
@@ -474,6 +607,8 @@ def extract_string(lines, header, continuationlength):
     otherlines = [line[continuationlength:-2] for line in lines[1:]]
     return b''.join([headerline] + otherlines)
 
+
+# Regular expression strings for .po file line patterns
 linepatternstrings = dict(comment=r'(#~ )?#[\s,\.:\|]|#~[,\.:\|]',
                           msgctxt=r'(#~ )?msgctxt ',
                           msgid=r'(#~ )?msgid ',
@@ -483,13 +618,21 @@ linepatternstrings = dict(comment=r'(#~ )?#[\s,\.:\|]|#~[,\.:\|]',
                           continuation=r'(#~ )?"',
                           prevmsgid_start=r'#\| msgid ',
                           prevmsgid_continuation=r'#\| "')
+# Compiled regular expressions for ordinary message line patters
 linepatterns = dict([(key, re.compile(value))
                      for key, value in linepatternstrings.items()])
+# Compiled regular expressions for obsolete message line patters
 obsolete_linepatterns = dict([(key, re.compile(r'#~( ?)' + value))
                               for key, value in linepatternstrings.items()])
 
 
 class PoParser:
+    """A .po file parser
+
+    Args:
+        input (file): A file like object in binary mode
+    """
+
     def __init__(self, input):
         self._input = input
         self.input = LineNumberIterator(input)
@@ -613,13 +756,23 @@ class PoParser:
 
 
 def parse(input):
+    """Parse .po file content from a file like object
+
+    Args:
+       input (file): A file like object in binary mode
+
+    Returns:
+        Catalog: A message catalog
+    """
     parser = PoParser(input)
-    
+
+    # Set filename
     try:
         fname = input.name
     except AttributeError:
         fname = '<unknown>'
 
+    # Split in chunks and obsoletes
     chunks = []
     obsoletes = []
 
@@ -629,7 +782,8 @@ def parse(input):
             obsoletes.append(chunk)
         else:
             chunks.append(chunk)
-    
+
+    # Find the header chunk
     for chunk in chunks:
         if chunk['msgid'] == '':
             header = chunk
@@ -637,6 +791,7 @@ def parse(input):
     else:
         raise PoHeaderError('Header not found')
 
+    # Determine the files true encoding from the gettext header
     for line in header['msgstrs'][0].split('\\n'):
         if line.startswith('Content-Type:'):
             break
@@ -644,26 +799,31 @@ def parse(input):
         if token.startswith('charset='):
             break
     encoding = token.split('=')[1]
-    
+
+    # Form the message as either ObsoleteMessage or Message
     msgs = []
     for chunk in chunks + obsoletes:
         msgstrs = chunk['msgstrs']
-        
+
+        # Make plural syntax sanity check
         if len(msgstrs) > 1:
             assert 'msgid_plural' in chunk
-        
+
+        # Form the metadata message dict in which rawlines is unicode
         meta = dict(rawlines=[line.decode(encoding)
                               for line in chunk['rawlines']],
                     lineno=chunk['lineno'],
                     fname=fname,
                     encoding=encoding)
-        
+
+        # Set message class
         if chunk['is_obsolete']:
             msgclass = ObsoleteMessage
         else:
             msgclass = Message
 
         def dec(txt):
+            """Return decoded version of txt or None if txt is None"""
             if isinstance(txt, basestring):
                 return txt.decode(encoding)
             elif txt is None:
@@ -671,6 +831,7 @@ def parse(input):
             else:
                 return txt.decode(encoding)
 
+        # Form the message instance, where all text (id, str, etc.) is unicode
         msg = msgclass(msgid=dec(chunk['msgid']),
                        msgstr=[dec(m) for m in msgstrs], # (includes plurals)
                        msgid_plural=dec(chunk.get('msgid_plural')),
@@ -681,6 +842,7 @@ def parse(input):
                        meta=meta)
         msgs.append(msg)
 
+    # Form the catalog
     cat = Catalog(fname, encoding, msgs)
     return cat
 
